2b3e77a2858f292f80a016c3dcc4e2b652e44780
nzlap <nz@gmail.com>
2026-03-05 19:20:37 +0700

    add some fix

--- models/note.js
+++ models/note.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
-const noteschema = new mongoose.schema({
+const noteSchema = new mongoose.Schema({
   content: {
-    type: string,
+    type: String,
     required: true,
   },
-  important: boolean,
+  important: Boolean,
   user: {
-    type: mongoose.schema.types.objectid,
-    ref: "user",
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
   },
 });
-noteschema.set("tojson", {
-  transform: (document, returnedobject) => {
-    returnedobject.id = returnedobject._id.tostring();
-    delete returnedobject._id;
-    delete returnedobject.__v;
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   },
 });
 
-const note = mongoose.model("note", noteschema);
-module.exports = note;
+const Note = mongoose.model("Note", noteSchema);
+module.exports = Note;
